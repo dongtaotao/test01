ea75bd593c9ea5fd407d53d1ec8f52d1c8450a7e
董涛（dongtao01）-移动客户端 <dongtao01@qiyi.com>
2021-10-28 17:26:27 +0800

    5

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React11 dev
+          Learn React11 dev 12
         </a>
       </header>
     </div>
